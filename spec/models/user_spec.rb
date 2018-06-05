79327d264f2b23daaa972640b83cdb4702cb1441
Elijah Rogers <elijahrogers@charter.net>
2018-06-05 00:02:49 -0700

    Refactor user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,27 +3,27 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many :projects }
 
-  it 'is database_authenticatable' do
+  it 'is database authenticatable' do
     expect(User.included_modules).to include(Devise::Models::DatabaseAuthenticatable)
   end
 
-  it 'is database_authenticatable' do
+  it 'is registerable' do
     expect(User.included_modules).to include(Devise::Models::Registerable)
   end
 
-  it 'is database_authenticatable' do
+  it 'is recoverable' do
     expect(User.included_modules).to include(Devise::Models::Recoverable)
   end
 
-  it 'is database_authenticatable' do
+  it 'is rememberable' do
     expect(User.included_modules).to include(Devise::Models::Rememberable)
   end
 
-  it 'is database_authenticatable' do
+  it 'is trackable' do
     expect(User.included_modules).to include(Devise::Models::Trackable)
   end
 
-  it 'is database_authenticatable' do
+  it 'is validatable' do
     expect(User.included_modules).to include(Devise::Models::Validatable)
   end
 end
